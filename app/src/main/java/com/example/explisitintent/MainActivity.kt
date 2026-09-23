95bf981f1170a8b891a4511e11a1c058ffd61d9b
clarenceevanw <clarenceevan0907@gmail.com>
2026-09-23 19:39:26 +0700

    feat: intent with send data class Pegawai

--- app/src/main/java/com/example/explisitintent/MainActivity.kt
+++ app/src/main/java/com/example/explisitintent/MainActivity.kt
@@ -20,6 +20,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val isiPegawai = Pegawai(1, "Solman", "Data Analyst")
+
         val _btnExplisit1 = findViewById<Button>(R.id.btnExplisit1)
         _btnExplisit1.setOnClickListener {
             val intent = Intent(
@@ -41,5 +43,17 @@ class MainActivity : AppCompatActivity() {
             }
             startActivity(intentWithData)
         }
+
+        val _btnExplisit3 = findViewById<Button>(R.id.btnExplisit3)
+        _btnExplisit3.setOnClickListener {
+            val intentWithData = Intent(
+                this@MainActivity,
+                MainActivity4::class.java
+            ).apply {
+                putExtra(MainActivity4.dataPegawai, isiPegawai)
+            }
+            startActivity(intentWithData)
+        }
+
     }
 }
